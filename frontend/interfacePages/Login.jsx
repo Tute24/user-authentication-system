dc445d4d75b47b6f2e5af20a3a0cdc93bbe90de3
Arthur Araujo <arthurtute200@hotmail.com>
2024-10-12 18:23:31 -0300

    feat: added the function handleLoginSubmit, that sends a post request to the backend for user authentication purposes

--- frontend/interfacePages/Login.jsx
+++ frontend/interfacePages/Login.jsx
@@ -1,18 +1,46 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Login(){
    
+const [loginUserData,setloginUserData] = useState({
+        email: "",
+        password:""
+})
+
+const [statusMessage,setStatusMessage] = useState('')
+
+function handleLoginInputChange(e){
+    setloginUserData({
+        ...loginUserData,
+        [e.target.name]: e.target.value
+    })
+}
+
+function handleLoginSubmit(e){
+    e.preventDefault()
+
+    try{
+        axios.post('/login',loginUserData)
+    } catch (error){
+        setStatusMessage(`There's something wrong with your request!`)
+    }
+}
+
     return(
         <>
         <div>
-            <form>
+            <form onSubmit={handleLoginSubmit}>
                 <label htmlFor="email">Your E-mail: </label>
-                <input type="email" id="email" name="user_email" placeholder="Log in with your e-mail"/>
+                <input type="email" id="email" name="email" value={loginUserData.email} placeholder="Log in with your e-mail" onChange={handleLoginInputChange}/>
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="user_password" placeholder="Type your password"/>
+                <input type="password" id="password" name="password" value={loginUserData.password} placeholder="Type your password" onChange={handleLoginInputChange}/>
                 <button type="submit" id="loginButton" >Log In</button>
             </form>
+        <div>
+            <span>{statusMessage}</span>
+        </div>
         </div>
         <div>
             <span>
