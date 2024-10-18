f92b1ef7891ff8cc6f74119e926e807b2414302c
Arthur Araujo <arthurtute200@hotmail.com>
2024-10-18 20:20:24 -0300

    feat:added a forms when clicking on the Update User Info button that will make a request to update the user's  email/password

--- frontend/interfacePages/Dashboard.jsx
+++ frontend/interfacePages/Dashboard.jsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react"
 export default function Dashboard(){
 
     const[display,setDisplay] = useState('')
+    const[displayUpdateForm,setDisplayUpdateForm] = useState(false)
+    const[updateUserData,setUpdateUserData]=useState({
+        name: '',
+        password: ''
+    })
     
     useEffect(()=>{
 
@@ -22,23 +27,66 @@ export default function Dashboard(){
         }catch(error){
             console.error("Error fetching data")
             setDisplay('Error')
-        }
-                 
-        
+        }   
     }
-
 }  
-
     fetchUserData()
     },[]
 )
 
+ function handleUpdateButton(){
+    setDisplayUpdateForm(true)
+}
 
+function handleUpdateInputChange(e){
+    setUpdateUserData({
+        ...updateUserData,
+        [e.target.name]: e.target.value
+    })
+}
+
+ async function handleUpdateSubmit(e){
+    e.preventDefault()
+
+    const token = JSON.parse(localStorage.getItem("token"))
+    if(token){
+    try{
+        const response = await axios.post('http://localhost:3000/update',updateUserData, {headers:
+        {
+            'Authorization': `Bearer ${token}`
+        }
+    }
+)
+    }catch(error){
+        console.log(error)
+    }}
+ }
 
 
     return (
         <>
-            <h1>Welcome to your dashboard page: {display}  </h1>
+            <h2>Welcome to your dashboard page: <span>{display}</span>  </h2>
+
+            <div>
+                <button type="button" onClick = {handleUpdateButton} >Update User Info</button>
+                <button type="button">Delete Account</button>
+            </div>
+
+            {displayUpdateForm &&(
+                <>
+                <div>
+                <p>Note: If you submit a different email/password than the one you're using before, they will be updated in the database. If you want to change only your email, for example, submit your new email address and your current password. The same goes for a password-only change.
+                </p>
+                </div>
+                <form onSubmit={handleUpdateSubmit}>
+                    <label htmlFor="email">Type your current email/new email</label>
+                    <input type="email" id="email" name="email" value={updateUserData.email} required onChange={handleUpdateInputChange}/>
+                    <label htmlFor="password">Type your current password/new password</label>
+                    <input type="password" id="password" name="password" value={updateUserData.password} required onChange={handleUpdateInputChange}/>
+                    <button type="submit">Submit Change</button>
+                </form>
+                </>
+            )}
         </>
     )
 }
