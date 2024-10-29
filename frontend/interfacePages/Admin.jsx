07241f1b3f3bd3d8317e2313b86d92637ee6f8d3
Arthur Araujo <arthurtute200@hotmail.com>
2024-10-28 22:04:14 -0300

    style:Styling the Admin page

--- frontend/interfacePages/Admin.jsx
+++ frontend/interfacePages/Admin.jsx
@@ -30,17 +30,22 @@ export default function Admin (){
 
     return(
         <>
-        <div>
-            <h2>
-                Admin Page
-            </h2>
-            <ul>
-                {userlist.map((e)=>(
-                    <li key={e._id}>
-                        <h3>Username: {e.username} ; Email: {e.email}</h3>
-                    </li>
-                ))}
-            </ul>
+        <div class='flex flex-col justify-center items-center min-h-screen bg-emerald-50'>
+            <div class='font-sans p-10 text-center bg-black text-white font-semibold rounded-2xl'>
+                <h2 class='text-green-400 p-2 -mt-2'>
+                    Welcome to the Admin Page
+                </h2>
+                <p class='text-amber-400 mb-3'>
+                    Users List:
+                </p>
+                <ul class='flex flex-col justify-center bg-emerald-50 p-2 rounded-2xl'>
+                    {userlist.map((e)=>(
+                        <li class='text-black' key={e._id}>
+                            <h3><span class='text-blue-500'> - Username:</span > "{e.username}" ; <span class='text-blue-500' >Email:</span> "{e.email}"</h3>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
         </>
     )
