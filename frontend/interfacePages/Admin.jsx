4cf20ffa02e18bb86718e00e7fdfef82639b592b
Arthur Araujo <arthurtute200@hotmail.com>
2024-10-29 22:28:28 -0300

    feat:implemented the logout function to the admin page

--- frontend/interfacePages/Admin.jsx
+++ frontend/interfacePages/Admin.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export default function Admin (){
     const [statusMessage,setStatusMessage] = useState('')
     const [userlist, setUserList] = useState([])
+
+    const navigate = useNavigate()
 
     useEffect(()=>{
         async function fecthDB(){
@@ -28,10 +31,28 @@ export default function Admin (){
     fecthDB()
     },[]) 
 
+    async function handleAdminLogout(){
+        const token = JSON.parse(localStorage.getItem('token'))
+
+        try{
+            const response = await axios.get('http://localhost:3000/adminlogout',{headers:{
+                'Authorization': `Bearer ${token}`
+            }})
+            localStorage.removeItem('token')
+            navigate('/')
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <>
         <div className='flex flex-col justify-center items-center min-h-screen bg-emerald-50'>
             <div className='font-sans p-10 text-center bg-black text-white font-semibold rounded-2xl'>
+                <nav className='flex flex-row-reverse justify-items-end mr-2'>
+                    <button className="  bg-red-600 text-xs rounded-full py-0.5 px-2 text-black font-bold border-red-500 border-solid border-2 -mt-1.5  hover:bg-red-900 focus:outline-none ring-2 ring-red-500" onClick={handleAdminLogout}>Logout</button>
+                </nav>
                 <h2 className='text-green-400 p-2 -mt-2'>
                     Welcome to the Admin Page
                 </h2>
