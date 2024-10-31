03df6c7c7f3c1545498da2dc380ebeab8e34c1f0
Arthur Araujo <arthurtute200@hotmail.com>
2024-10-30 23:22:39 -0300

    deploy:adding cors config

--- backend/App/app.cjs
+++ backend/App/app.cjs
@@ -18,7 +18,11 @@ mongoose.connect(process.env.MONGODB_URI,{
     useUnifiedTopology:true,
 })
 
-app.use(cors())
+app.use(cors({
+    origin:["https://userauth-frontend.vercel.app/"],
+    methods: ["POST","GET"],
+    credentials: true
+}))
 app.use(express.json())
 
 app.use(loginRouter)
