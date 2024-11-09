e0d23bad1994f8c6ea2eca948c8d195cbcf5cd83
Arthur Araujo <arthurtute200@hotmail.com>
2024-11-09 15:50:48 -0300

    feat: add a diolog box in the admin pags that requests confirmation when the admin selects to give admin or delete an user

--- frontend/interfacePages/Admin.jsx
+++ frontend/interfacePages/Admin.jsx
@@ -12,10 +12,13 @@ export default function Admin (){
     const [deletedUser,setDeletedUser] = useState({
         deletedEmail: ''
     })
+    const [gotEmail,setGotEmail] = useState('')
+    const [gotUser, setGotUSer] = useState('')
 
     const navigate = useNavigate()
     const apiUrl = import.meta.env.VITE_API_URL
-    const emailRef = useRef(null)
+    const dialogRefAdmin = useRef(null)
+    const dialogRefDelete = useRef(null)
 
     useEffect(()=>{
         async function fecthDB(){
@@ -39,21 +42,7 @@ export default function Admin (){
     fecthDB()
     },[]) 
 
-    useEffect(() => { 
-        async function giveAdmin(){
-        const token = JSON.parse(localStorage.getItem('token'))
     
-        if(token){
-            try{
-                const response = await axios.post(`${apiUrl}giveAdmin`,emailObject,{headers:{
-                    'Authorization': `Bearer: ${token}`
-                }})
-            }catch(error){
-                console.log(error)
-            }
-        }}
-        giveAdmin()
-    }, [emailObject]);
 
     async function handleAdminLogout(){
         const token = JSON.parse(localStorage.getItem('token'))
@@ -69,6 +58,38 @@ export default function Admin (){
             console.log(error)
         }
     }
+
+    function openAdminDialog(){
+        dialogRefAdmin.current.showModal()
+    }
+
+    function closeAdminDialog(){
+        dialogRefAdmin.current.close()
+    }
+
+    function openDeleteDialog(){
+        dialogRefDelete.current.showModal()
+    }
+
+    function closeDeleteDialog(){
+        dialogRefDelete.current.close()
+    }
+
+    useEffect(() => { 
+        async function giveAdmin(){
+        const token = JSON.parse(localStorage.getItem('token'))
+    
+        if(token){
+            try{
+                const response = await axios.post(`${apiUrl}giveAdmin`,emailObject,{headers:{
+                    'Authorization': `Bearer: ${token}`
+                }})
+            }catch(error){
+                console.log(error)
+            }
+        }}
+        giveAdmin()
+    }, [emailObject])
 
     useEffect(()=>{
         async function handleDeletedUser() {
@@ -101,25 +122,46 @@ export default function Admin (){
                 <ul className='flex flex-col justify-center bg-emerald-50 p-2 rounded-2xl text-sm'>
                     {userlist.map((e)=>(
                         <li className='text-black flex flex-row p-2' key={e._id}>
-                            <h3><span className='text-blue-500'> - Username:</span > "{e.username}" ; <span className='text-blue-500' >Email:</span> "<span ref={emailRef}>{e.email}</span>"</h3>
-                            <button type="button" class=" bg-red-600 text-xs rounded-full py-0.5 px-2 text-black font-bold border-red-500 border-solid border-2 -mt-1.5" onClick={async()=>{
-                                const deletedUserEmail = e.email
-                                setDeletedUser({
-                                    deletedEmail: deletedUserEmail
-                                })
-                            }}>Delete User</button>
-                            <button type="button" class=" bg-red-600 text-xs rounded-full py-0.5 px-2 text-black font-bold border-red-500 border-solid border-2 -mt-1.5" onClick={async()=>{
-
-                                const correspondentEmail = e.email
-                                 setEmailObject({
-                                    fetchedEmail: correspondentEmail
-                                }
-                            )
-                                
-                            }}>Give Admin</button>
+                            <h3><span className='text-blue-500'> - Username:</span > "{e.username}" ; <span className='text-blue-500' >Email:</span> "<span >{e.email}</span>"</h3>
+                            <button type="button" class=" bg-red-600 text-xs rounded-full py-0.5 px-2 text-black font-bold border-red-500 border-solid border-2 -mt-1.5" onClick={ ()=>{
+                                setGotEmail(e.email)
+                                setGotUSer(e.username);
+                                openDeleteDialog()} 
+                            }>Delete</button>
+                            <button type="button" class=" bg-red-600 text-xs rounded-full py-0.5 px-2 text-black font-bold border-red-500 border-solid border-2 -mt-1.5" 
+                            onClick={()=>{
+                                setGotEmail(e.email)
+                                setGotUSer(e.username);
+                                openAdminDialog()} 
+                            } 
+                            >Admin</button>
                         </li>
                     ))}
                 </ul>
+                <dialog ref={dialogRefDelete}>    
+                <button type="button" onClick={closeDeleteDialog} className="bg-cyan-500">Close</button>
+                <p>Are you sure you want to delete <span className="text-red-700">{gotUser}</span> ?</p>
+                    <button type="button" onClick={()=>{
+                        setDeletedUser({
+                            deletedEmail: gotEmail
+                        });
+                        closeDeleteDialog()
+                    }}>Delete User</button>
+                </dialog>
+
+                <dialog ref={dialogRefAdmin}>
+                <div className="p-5">
+                <button type="button" onClick={closeAdminDialog} className="bg-cyan-500">Close</button>
+                    <p>Are you sure you want to give <span className="text-red-700">{gotUser}</span> an admin role ?</p>
+                    <button type="button" onClick={()=>{
+                        setEmailObject({
+                            fetchedEmail: gotEmail
+                        });
+                        closeAdminDialog()
+                    }}>Give Admin</button>
+                </div>
+                </dialog>
+                
             </div>
         </div>
         </>
